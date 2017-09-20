0b760ecdb3bd50fb3ba5d9bdd75687b2d47e9278
shoegazerwithak <skrtroot@protonmail.com>
2017-09-20 10:56:17 +0300

    update MovieList

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,32 +1,57 @@
 import * as React from "react";
 import Movie from "../Movie";
 import "./MovieList.css";
-
-interface MovieType {
-  poster_path?: string | null;
-  title: string;
-}
+import { MovieType } from "../../store/reducers";
+import { isBottomOfElement } from "../../utils/index";
+import { Cancelable, debounce } from "lodash";
 
 interface Props {
   items?: Array<Object>;
   onEndReached?(): void;
+  // onEndReached?: (()=>void) & Cancelable;
+  onFav?(id: number): void;
 }
 
 class MovieList extends React.Component<Props> {
   public static defaultProps: Props = {
     items: [],
-    onEndReached: () => {}
+    onEndReached: () => {},
+    // onEndReached: debounce(() => {}, 1000),
+    onFav: () => {}
   } as Props;
+  scrollable: HTMLDivElement | null = null;
   state = {};
 
+  onScroll = () => {
+    if (isBottomOfElement(this.scrollable)) {
+      const { onEndReached = () => {} } = this.props;
+      onEndReached();
+    }
+  };
+
+  handleFav = (id: number) => (this.props.onFav as (id: number) => void)(id);
+
+  // >).map(({ poster_path: image, title, id, fav, credit_id = "" }) => (
   render() {
     return (
-      <div className={"MovieList"}>
+      <div
+        ref={scrollable => (this.scrollable = scrollable)}
+        onScroll={this.onScroll}
+        className={"MovieList"}
+      >
         {(this.props.items as Array<
           MovieType
-        >).map(({ poster_path: image, title }) => (
-          <Movie key={title} image={image} title={title} />
-        ))}
+        >).map(({ poster_path: image, title, id, fav, credit_id = "" }) => {
+          return (
+            <Movie
+              fav={fav}
+              onFav={() => this.handleFav(id)}
+              key={id + credit_id}
+              image={image}
+              title={title}
+            />
+          );
+        })}
       </div>
     );
   }
