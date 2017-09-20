92da45fdea5dafb39877fba40d40c5d86817a859
shoegazerwithak <skrtroot@protonmail.com>
2017-09-20 10:41:37 +0300

    Update Movie component

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,9 +1,12 @@
-import * as React from 'react';
-import './Movie.css';
+import * as React from "react";
+import fav from "../../assets/ic_favorite_border_black_24px.svg";
+import "./Movie.css";
 
 interface Props {
   image?: string | null;
   title?: string;
+  fav?: boolean;
+  onFav?(): void;
 }
 
 class Movie extends React.Component<Props> {
@@ -12,10 +15,24 @@ class Movie extends React.Component<Props> {
     return image ? `https://image.tmdb.org/t/p/w342/${image}` : undefined;
   }
 
+  get className() {
+    return `Movie-hide ${this.props.fav ? "Movie-show" : ""}`;
+  }
+
   render() {
     return (
       <div className="Movie">
-        <img alt="" className={'MovieImage'} src={this.source} />
+        <div className="Movie-hide-absolute">
+          <img
+            onClick={this.props.onFav}
+            src={fav}
+            alt="fav"
+            className={this.className}
+          />
+        </div>
+        {this.source && (
+          <img alt="" className={"MovieImage"} src={this.source} />
+        )}
       </div>
     );
   }
