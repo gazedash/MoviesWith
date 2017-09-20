d1b4db8515c5cfd3b3852c54ba8372f628f8cb9b
shoegazerwithak <skrtroot@protonmail.com>
2017-09-20 10:52:51 +0300

    update index.tsx: subscribe to store

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import store from './store';
-import api from './api';
-import './index.css';
-const res = api.fetchMovies({ with_cast: 287 });
-res.then((data) => console.log(data));
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import store from "./store";
+import { saveState } from "./utils/index";
+import "./index.css";
+
+store.subscribe(() => {
+  saveState({ fav: store.getState().fav, movies: store.getState().movies });
+});
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 registerServiceWorker();
