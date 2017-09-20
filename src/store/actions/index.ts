0bef2de41da5a3fcbd9d86f6fcf0fbbe96a0a926
shoegazerwithak <skrtroot@protonmail.com>
2017-09-20 10:51:02 +0300

    update store: actions, reducers

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,27 +1,156 @@
-import { Action, Dispatch } from "redux";
+import { Dispatch } from "redux";
+import { IState, Actor } from "../reducers";
 import * as api from "../../api";
 import { ThunkAction } from "redux-thunk";
+import { createAction, ComplexActionCreator1 } from "redux-act";
 
-// export const FetchActor = {
-//     type: 'FETCH_ACTOR',
-//     action(params: Object = {}) { return { type: this.type, ...params } as Action; },
-//     thunk()
-// };
-export const FetchActor = {
-  type: "FETCH_ACTOR",
-  action(args: Object = { name: "" }) {
-    return async (dispatch: Dispatch<{}>) => {
-      const data = await api.fetchActor(name);
-      return dispatch({ type: this.type, payload: data });
+export class FetchActor {
+  type = "FETCH_ACTOR";
+  action(idOrName?: string | number) {
+    interface Response {
+      results?: Array<Object>;
+    }
+    return async (dispatch: Dispatch<{}>, getState: () => IState) => {
+      const { actors } = getState();
+      let payload = actors.find(
+        actor =>
+          actor.name === idOrName || actor.id === parseInt(<string>idOrName, 10)
+      );
+      if (!payload) {
+        const {
+          results = []
+        }: Response = await api.fetchActor(<string>idOrName);
+        payload = <Actor>results[0];
+      }
+      return dispatch({ type: this.type, payload });
     };
-    // return { type: this.type, name } as Action;
   }
-};
+}
+
+interface IDefaultAction {
+  type: string;
+  action(payload?: {}): {};
+}
+
+export interface Action {
+  type: string;
+  payload?: {} | Array<{}> | null;
+}
+
+export class DefaultAction implements IDefaultAction {
+  type = "DEFAULT_ACTION";
+  action(payload?: {}) {
+    return { type: this.type, payload: payload ? payload : {} };
+  }
+}
+
+export class SetActorMoviesSearchParams extends DefaultAction {
+  type = "SET_ACTOR_MOVIES_SEARCH_PARAMS";
+}
+export class RemoveActor extends DefaultAction {
+  type = "REMOVE_ACTOR";
+}
+export class SetCurrentActor extends DefaultAction {
+  type = "SET_CURRENT_ACTOR";
+}
+export class ToggleFav extends DefaultAction {
+  type = "TOGGLE_FAV";
+}
+export class ShowFavs extends DefaultAction {
+  type = "SHOW_FAVS";
+}
+export class SwitchByAllActors extends DefaultAction {
+  type = "SWITCH_BY_ALL_ACTORS";
+}
+export class SwitchType extends DefaultAction {
+  type = "SWITCH_TYPE";
+}
+export class FetchNext {
+  type = "FETCH_NEXT";
+  action() {
+    return async (dispatch: Dispatch<{}>, getState: () => IState) => {
+      const { current, searchParams, actors, showFavs, type } = getState();
+      if (!showFavs || type === "movie") {
+        const lastId = actors.length - 1;
+        let { id = null } = actors[lastId];
+        id = current ? current : id;
+        const ids = (id || "").toString();
+        const { total_pages = 1, page = 1 } = searchParams[ids] || {};
+        if (total_pages > page && id) {
+          const args = { with_cast: ids, page: page + 1, setCurrent: false };
+          dispatch({ type: this.type, ...args });
+          return dispatch(new FetchMovies().action(args));
+        }
+      }
+    };
+  }
+}
+
+export class FetchMovies {
+  type = "FETCH_MOVIES";
+  action(args: {} = {}) {
+    interface Args {
+      with_cast?: Array<number> | number | string;
+      page?: number;
+      setCurrent?: boolean;
+      stop?: boolean;
+      type?: "tv" | "movie" | "combined";
+    }
+    interface Response {
+      results?: Array<Object>;
+      cast?: Array<Object>;
+    }
+
+    let { with_cast, setCurrent = true, stop = false, ...rest }: Args = args;
+    return async (dispatch: Dispatch<{}>, getState: () => IState) => {
+      let { type } = getState();
+      if (rest.type) {
+        type = rest.type;
+      }
+      if (typeof with_cast === "string") {
+        const { payload: { id = 0 } = {} } = await dispatch(
+          new FetchActor().action(with_cast)
+        );
+        with_cast = id;
+      }
+      if (with_cast) {
+        const ids = with_cast.toString();
+        let {
+          results = [],
+          cast = [],
+          ...searchParams
+        }: Response = await api.fetchMovies({
+          with_cast: ids,
+          type,
+          ...rest || {}
+        });
+        // dispatch searchParams
+        dispatch(
+          new SetActorMoviesSearchParams().action({ ids, searchParams })
+        );
+        if (setCurrent) {
+          dispatch(new SetCurrentActor().action(with_cast));
+        }
+        const castArray = (<number[]>[]).concat(with_cast);
+        if (type !== "movie") {
+          results = cast;
+        }
+        const payload = results.map(movie => {
+          return { media_type: type, ...movie, cast: castArray };
+        });
+        if (type === 'movie' && !stop) {
+          dispatch(new FetchMovies().action({ with_cast, setCurrent: false, type: 'tv', stop: true }))
+        }
+        return dispatch({ type: this.type, payload });
+      }
+    };
+  }
+}
 
 export default {
-    FetchActor
+  FetchActor,
+  FetchMovies,
+  FetchNext,
+  SetActorMoviesSearchParams,
+  SetCurrentActor
 };
-
-// if (action.type === FetchSelectedUsers.type) {
-// FetchSelectedUsers.action();
-// }
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,21 +1,155 @@
 import { combineReducers } from "redux";
 import { createReducer } from "reduxsauce";
 import * as Actions from "../actions";
-import data from './data.json';
+import R from "ramda";
+import data from "./data.json";
 // the initial state of this reducer
-export const INITIAL_STATE = data;
-// the eagle has landed
-export const success = (state = INITIAL_STATE, action = {}) => {
-  return { ...state, error: false, goodies: action };
+export interface MovieType {
+  poster_path?: string | null;
+  media_type?: string;
+  title: string;
+  credit_id?: string;
+  cast: Array<number>;
+  fav?: boolean;
+  id: number;
+}
+export interface SearchParams {
+  total_pages: number;
+  page: number;
+}
+export interface Actor {
+  id: number;
+  profile_path: string | null;
+  name: string;
+  // for cases when showing movies by all actors
+  names: string;
+}
+export interface IState {
+  movies: Array<MovieType>;
+  actors: Array<Actor>;
+  current: number;
+  searchParams: { [id: string]: SearchParams };
+  showFavs: boolean;
+  shouldLoadByAll: boolean;
+  fav: { [id: number]: boolean };
+  type: "movie" | "tv" | "combined";
+  types: typeof types;
+}
+// export const INITIAL_STATE = [];
+export const movie = (
+  state = <MovieType[]>[],
+  { payload = <MovieType[]>[] } = {}
+) => {
+  const newMovies = <MovieType[]>[];
+  const movies: { [index: number]: MovieType } = {};
+  // O(M*N)
+  payload.forEach(item => {
+    const index = state.findIndex(oldMovie => oldMovie.id === item.id);
+    if (index >= 0) {
+      movies[index] = item;
+    } else {
+      newMovies.push(item);
+    }
+  });
+  // O(N)
+  const newState = state.map((item, index) => {
+    if (movies[index]) {
+      let concatCast = (k: string, l: number[], r: number[]) =>
+        k === "cast" ? R.concat(l, r) : r;
+      return R.mergeWithKey(concatCast, item, movies[index]);
+    }
+    return item;
+  });
+  // there were response with two identical ids
+  return R.unionWith(R.eqBy(R.prop("id")), newState, newMovies);
+};
+
+export const currentActor = (state = null, { payload = null } = {}) => {
+  if (state === payload) {
+    return null;
+  }
+  return payload;
+};
+
+export const actor = (state = [], { payload = {} } = {}) => {
+  if (R.contains(payload, state)) {
+    return state;
+  }
+  return R.append(payload, state);
+};
+
+export const removeActor = (state = <Actor[]>[], { payload: id = 0 } = {}) => {
+  return state.filter(actorItem => actorItem.id !== id);
+};
+
+const types = ["tv", "movie", "combined"];
+export const type = (state = "movie", { payload = "movie" } = {}) => {
+  if (types.includes(payload)) {
+    return payload;
+  }
+  return state;
+};
+
+export const searchParam = (
+  state = {},
+  { payload: { ids = "", searchParams = {} } = {} } = {}
+) => {
+  // return assocPath([ids], searchParams, state);
+  return { ...state, [ids]: searchParams };
+};
+
+export const fav = (
+  state: { [id: number]: boolean } = {},
+  { payload: id = 0 } = {}
+) => {
+  return { ...state, [id]: !state[id] };
+};
+
+export const showFavs = (state = false, {}) => {
+  return !state;
+};
+
+export const switchByAllActors = (state = false, {}) => {
+  return !state;
 };
 
 // map our action types to our reducer functions
-export const HANDLERS = {
-  [Actions.FetchSelectedUsers.type]: success
+// [new Actions.FetchNext().type]: success,
+export const MOVIE_HANDLERS = {
+  [new Actions.FetchMovies().type]: movie
+};
+export const CURRENT_HANDLERS = {
+  [new Actions.SetCurrentActor().type]: currentActor
+};
+export const ACTOR_HANDLERS = {
+  [new Actions.FetchActor().type]: actor,
+  [new Actions.RemoveActor().type]: removeActor
+};
+export const SEARCH_PARAMS_HANDLERS = {
+  [new Actions.SetActorMoviesSearchParams().type]: searchParam
+};
+export const FAV_HANDLERS = {
+  [new Actions.ToggleFav().type]: fav
+};
+export const SHOW_FAVS_HANDLERS = {
+  [new Actions.ShowFavs().type]: showFavs
+};
+export const SWITCH_BY_ALL_ACTORS = {
+  [new Actions.SwitchByAllActors().type]: switchByAllActors
+};
+export const SWITCH_TYPE = {
+  [new Actions.SwitchType().type]: type
 };
 
-const movies = createReducer(INITIAL_STATE, HANDLERS);
-
-export default combineReducers({
-  movies
-});
+const storeState = {
+  movies: createReducer([], MOVIE_HANDLERS),
+  current: createReducer(NaN, CURRENT_HANDLERS),
+  actors: createReducer([], ACTOR_HANDLERS),
+  searchParams: createReducer({}, SEARCH_PARAMS_HANDLERS),
+  fav: createReducer({}, FAV_HANDLERS),
+  showFavs: createReducer(false, SHOW_FAVS_HANDLERS),
+  shouldLoadByAll: createReducer(false, SWITCH_BY_ALL_ACTORS),
+  type: createReducer("movie", SWITCH_TYPE),
+  types: () => types
+};
+export default combineReducers(storeState);
